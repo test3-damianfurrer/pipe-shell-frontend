402040b1cf78159ea25092c22cb2741a3dc6cf2f
210mlt <210mlt@gmail.com>
2022-09-03 09:58:50 +0200

    moved menu dependency to sh/_menu Changed this to use bemenu or similar, instead of dmenu

--- doplaylistid.sh
+++ doplaylistid.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 source sh/prcplaylist.src
+source sh/_menu.src
 #prcplaylist "PLUlK5tPwfK5ICvNt_3XyFrsu_BE2NerpJ" # $1
 if [ "$1" != "" ]; then
   prcplaylist "$1"
 else
-  prcplaylist $(cat play.list | dmenu -l 10 -p 'list' | awk '{print $1}' FS=';')
+  #prcplaylist $(cat play.list | dmenu -l 10 -p 'list' | awk '{print $1}' FS=';')
+  prcplaylist $(cat play.list | _menu 10 'list' | awk '{print $1}' FS=';')
 fi
