41eccc3d34b1bec9ebd20ff471d62b842a1cf139
210mlt <210mlt@gmail.com>
2022-09-05 13:36:17 +0200

    added random video selection mode

--- playviewall.sh
+++ playviewall.sh
@@ -19,6 +19,7 @@ declare -i i
 i=1
 while [ $i -le 100 ]
 do
+ ##newest, random, shortest
   id=$(selectavid "${folder}" "newest" $i) || exit 1
   prcvideo "${id}" "view+{;}qualselect{;}480p{x}530106{x}MPEG_4{v-a}126 kbps{x}132459{x}M4A{;}next" || retc=$?
   i+=1
