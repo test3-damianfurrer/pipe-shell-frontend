16e2699544e3490b149715e332382f64dec3dd7f
210mlt <210mlt@gmail.com>
2022-09-05 11:32:27 +0200

    added auto listen/view for video lists

---
+++ playlistenall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#$1 channelname
+./sh/update-ifseq.sh
+
+source ./sh/selectavid.src
+source ./sh/prcvideo.src
+
+folder=$(echo "${1}" | sed 's/\/$//')
+if [ "$folder" == "" ]; then
+        #>&2 echo "Pass name!"
+        #exit 1
+        folder="videos"
+fi
+./update.sh &
+retc=0
+#while [ $retc -eq 0 ]
+#do
+declare -i i
+i=1
+while [ $i -le 100 ]
+do
+  id=$(selectavid "${folder}" "newest" $i) || exit 1
+  prcvideo "${id}" "view+{;}listen{;}126 kbps{x}132459{x}M4A{;}next" || retc=$?
+  i+=1
+done
+#done
