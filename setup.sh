2abb728b4e7aeb63aa9febbae84a3983b9e19228
master <test3@damianfurrer.ch>
2023-03-28 00:28:14 +0200

    fixed setup

--- setup.sh
+++ setup.sh
@@ -3,11 +3,17 @@ set -e
 ln -sf do.sh play
 ln -sf do.sh listenall
 ln -sf do.sh update
+
+newpath=$(echo "$PWD/_c/duration2text" | sed 's/\//\\\//g')
+sed 's/_duration2text_path_/'"${newpath}"'/' sh/_thumbdl.src.tmpl > sh/_thumbdl.src
+source sh/_thumbdl.src
+
+cd _c
+gcc -o duration2text duration2text.c
+cd ..
+
 echo "UCf93fPKwotph47H3_KDcRyg" > abos.lst
 ./update
 echo "PLDIoUOhQQPlXqz5QZ3dx-lh_p6RcPeKjv" > pllists.lst
 ./test.sh "upd_playlist"
-newpath=$(echo "$PWD/_c/duration2text" | sed 's/\//\\\//g')
-sed 's/_duration2text_path_/'"${newpath}"'/' sh/_thumbdl.src.tmpl > sh/_thumbdl.src
-cd _c
-gcc -o duration2text duration2text.c
+
