47e687d23636be47cf7425f8ef2621507faa4a21
master <test3@damianfurrer.ch>
2023-03-28 00:24:18 +0200

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 ln -sf do.sh play
 ln -sf do.sh listenall
 ln -sf do.sh update
@@ -7,6 +8,6 @@ echo "UCf93fPKwotph47H3_KDcRyg" > abos.lst
 echo "PLDIoUOhQQPlXqz5QZ3dx-lh_p6RcPeKjv" > pllists.lst
 ./test.sh "upd_playlist"
 newpath=$(echo "$PWD/_c/duration2text" | sed 's/\//\\\//g')
-sed -i 's/_duration2text_path_/'"${newpath}"'/' sh/_thumbdl.src
+sed 's/_duration2text_path_/'"${newpath}"'/' sh/_thumbdl.src.tmpl > sh/_thumbdl.src
 cd _c
 gcc -o duration2text duration2text.c
