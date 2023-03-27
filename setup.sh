dcf06946ddc1d19a7b396f82991952c3dffc79dd
master <test3@damianfurrer.ch>
2023-03-28 00:48:00 +0200

    check required programs in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+chechprogramavailable(){
+  which "$1" 2>/dev/null >/dev/null || echo "Program '$1' must be installed!" >&2
+  which "$1" 2>/dev/null >/dev/null || exit 1
+}
 set -e
+
+#some programs might have alternatives and could be easy to change in the sh/_* files
+#chechprogramavailable "x" - test
+chechprogramavailable "jq"
+chechprogramavailable "ffmpeg"
+chechprogramavailable "mpv"
+chechprogramavailable "dmenu"
+chechprogramavailable "xmessage"
+chechprogramavailable "xclip"
+
+exit
+
 ln -sf do.sh play
 ln -sf do.sh listenall
 ln -sf do.sh update
