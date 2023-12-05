ae613792a4c7d626333ee48756447b6fb42cc9bd
master <test3@damianfurrer.ch>
2023-12-06 00:48:07 +0100

    Added silent update fview fixed updated .gitignore

--- sh/update-seq.sh
+++ sh/update-seq.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+[ "$1" != "" ] && source ./sh/update-seq-silent.src
+[ "$1" != "" ] && grep -v '#' abos.lst | update_seq
+[ "$1" != "" ] && exit
+
 source ./sh/update-seq.src
 
 grep -v '#' abos.lst | update_seq
